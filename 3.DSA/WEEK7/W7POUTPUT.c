092afd8b76206319cbf0411ce04c53e8e6061adc
Juthesh kumar sunkara <115220478+Juthesh6666@users.noreply.github.com>
2022-10-29 09:54:52 +0530

    Add files via upload

---
+++ 3.DSA/WEEK7/W7POUTPUT.c
@@ -0,0 +1,83 @@
+W7P1
+
+1. Insert, 2. Delete, 3. Display, -1. Exit
+Enter choice. 1
+Enter string to insert 12
+
+Enter choice. 1
+Enter string to insert 12
+
+Enter choice. 1
+Enter string to insert 12
+
+Enter choice. 2
+Deleted string: 12
+Enter choice. 3
+12 12
+Enter choice. -1
+exiting
+
+W7P2
+
+Enter n 12
+
+1. Insert1, 2. Insert2, 3. Delete1, 4. Delete2, 5. Display, -1. Exit
+Enter choice. 1
+Enter number to insert 12
+
+Enter choice. 2
+Enter number to insert 13
+
+Enter choice. 2
+Enter number to insert 14
+
+Enter choice. 4
+Element deleted is: 13
+Enter choice. 5
+
+Queue 1: 12
+Queue 2: 14
+Enter choice. -1
+exiting
+
+W7P3
+
+Enter n 5
+Enter elements of queue
+Enter number to insert 1
+Enter number to insert 2
+Enter number to insert 3
+Enter number to insert 4
+Enter number to insert 5
+Enter number to search for 3
+Element is at index: 2
+The queue is: 1 2 3 4
+
+
+W7P4
+
+1. Push
+2. Pop
+3. Exit
+1
+Enter element : 12
+1. Push
+2. Pop
+3. Exit
+1
+Enter element : 14
+1. Push
+2. Pop
+3. Exit
+2
+Popped : 12
+1. Push
+2. Pop
+3. Exit
+2
+Popped : 14
+1. Push
+2. Pop
+3. Exit
+3
+exiting 
